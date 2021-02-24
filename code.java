feea8c4591cf859e8598cdabb9641cb8a763a26a
Warrior Atrey <65439026+WarriorAtrey@users.noreply.github.com>
2021-02-24 16:04:31 +0530

    code.java

---
+++ code.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class HelloWorld
+{ 
+    public static void main() 
+    { 
+Scanner xxx = new Scanner(System.in);  
+System.out.println(" 1 For M , 2 For F & 0 For other ");
+int x = xxx.nextInt(System.in);
+if (x==1)
+System.out.println("Fuck Off")
+else if (x==2)
+System.out.println("You are beautiful af , follow me on ig @anshumanatrey ❤❤");
+else if (x==3)
+System.out.println("Universal Error");
+else 
+System.out.println("Hello World");
+    } 
+    }
